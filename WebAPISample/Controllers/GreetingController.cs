4abbdaed8eb6353621857e12d0403fec5cce6e22
tavisca-rsingh <rsingh@tavisca.in>
2019-08-13 18:25:47 +0530

    Completed Web API.

---
+++ WebAPISample/Controllers/GreetingController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPISample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GreetingController : ControllerBase
+    {
+        [HttpGet("{greeting}")]
+        public ActionResult<string> Get(string greeting)
+        {
+            return greeting == "hi" ? "hello" : greeting == "hello" ? "hi" : "Invalid";
+        }
+    }
+}
---
+++ WebAPITest/UnitTest1.cs
@@ -0,0 +1,31 @@
+using System;
+using WebAPISample.Controllers;
+using Xunit;
+
+namespace WebAPITest
+{
+    public class UnitTest1
+    {
+        private GreetingController _greeting = new GreetingController();
+        [Fact]
+        public void Test_For_Hi()
+        {
+            var response = _greeting.Get("hi").Value;
+            Assert.Equal("hello", response);
+        }
+
+        [Fact]
+        public void Test_For_Hello()
+        {
+            var response = _greeting.Get("hello").Value;
+            Assert.Equal("hi", response);
+        }
+
+        [Fact]
+        public void Test_For_Garbage()
+        {
+            var response = _greeting.Get("garbageValue").Value;
+            Assert.Equal("Invalid", response);
+        }
+    }
+}
